60568c43fd29252d16f4af4a80d7d8d18cdcf4cb
Mukanahallipathna Satish Kedarnag <kedarnag_mukanahalli_from.tebs@stb.gov.sg>
2019-09-01 15:28:28 +0800

    [development implement_users] :white_check_mark: Adds test cases for listing users.

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -11,6 +11,14 @@ describe Api::V1::UsersController, type: :request do
       expect(response_body).to match_json_expression(expected_json)
     end
 
+    it "should not list users if none are present" do
+      get "/api/v1/users", params: {}
+      response_body = JSON.parse(response.body)
+      expect(response_body["status"]["code"]).to eq(204)
+      expect(response_body["status"]["errorDetail"]).to eq("Nothing to display")
+      expect(response_body["status"]["message"]).to eq("NO CONTENT")
+    end
+
   end
 
 end
