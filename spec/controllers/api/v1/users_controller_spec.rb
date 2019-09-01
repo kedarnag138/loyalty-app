30fced75f81c16f2bcb32fafeea6f95704b42484
Mukanahallipathna Satish Kedarnag <kedarnag_mukanahalli_from.tebs@stb.gov.sg>
2019-09-01 15:33:54 +0800

    [development implement_users] :white_check_mark: Adds test cases for creating users.

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -21,4 +21,40 @@ describe Api::V1::UsersController, type: :request do
 
   end
 
+  context "POST #create" do
+
+    it "should be able to create a user" do
+      user_params = {
+        "user": {
+          first_name: Faker::Name.first_name,
+          last_name: Faker::Name.last_name,
+          birthday: Faker::Date.birthday(min_age: 1, max_age: 99),
+          email: Faker::Internet.email
+        }
+      }
+      post "/api/v1/users", params: user_params
+      response_body = JSON.parse(response.body)
+      expect(response_body["status"]["code"]).to eq(200)
+      expect(response_body["status"]["errorDetail"]).to eq("")
+      expect(response_body["status"]["message"]).to eq("OK")
+    end
+
+    it "should not able to create a user if validation fails" do
+      user_params = {
+        "user": {
+          first_name: Faker::Name.first_name,
+          last_name: Faker::Name.last_name,
+          birthday: "",
+          email: Faker::Internet.email
+        }
+      }
+      post "/api/v1/users", params: user_params
+      response_body = JSON.parse(response.body)
+      expect(response_body["status"]["code"]).to eq(422)
+      expect(response_body["status"]["errorDetail"]).to eq("Birthday can't be blank")
+      expect(response_body["status"]["message"]).to eq("VALIDATION FAILED")
+    end
+
+  end
+
 end
