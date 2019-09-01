409acd7bce255df598015ca19c8e3c93f2a3e5d0
Mukanahallipathna Satish Kedarnag <kedarnag_mukanahalli_from.tebs@stb.gov.sg>
2019-09-01 16:08:01 +0800

    [development implement_transactions] :sparkles: Adds API to create transactions.

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -4,4 +4,14 @@ class Transaction < ApplicationRecord
   belongs_to :user
 
   geocoded_by latitude: :latitude, longitude: :longitude
+
+  after_create :generate_points_based_on_location
+
+  def generate_points_based_on_location
+    latitude = self.latitude
+    longitude = self.longitude
+    latitude_longitude = "#{latitude},#{longitude}"
+    response = Geocoder.search(latitude_longitude).first
+  end
+
 end
